209f29b56404cad3801f8e535fe3a8cb997ff21f
phi <git@proffitt.io>
2020-06-13 22:38:39 -0500

    function name change

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn greet() {
+pub fn start() {
     let window = web_sys::window().expect("No global window exists!");
     let _ = window.alert_with_message(get_str());
     web_sys::console::log_1(&get_str().into());
 }
 
 fn get_str() -> &'static str {
-    return "What's up?";
+    return "Deploy Test!";
 }
