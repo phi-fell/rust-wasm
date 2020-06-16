04c2a5290e94b040900ed42ff5e242ccc57bfff9
phi <git@proffitt.io>
2020-06-16 09:25:47 -0500

    dom manipulation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[wasm_bindgen]
-pub fn start() {
-    let window = web_sys::window().expect("No global window exists!");
-    let _ = window.alert_with_message(get_str());
-    web_sys::console::log_1(&get_str().into());
+pub fn start_game() {
+    app_main().expect("Application Crashed!");
 }
 
-fn get_str() -> &'static str {
-    return "Deploy Test!";
+fn app_main() -> Result<(), JsValue> {
+    web_sys::console::log_1(&"Starting app...".into());
+    let window = web_sys::window().expect("No global window exists!");
+    let document = window.document().expect("No window document exists!");
+    let body = document
+        .create_element("body")?
+        .dyn_into::<web_sys::HtmlElement>()?;
+    document.set_body(Some(&body));
+
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
 }
